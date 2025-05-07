0af300e03bfdf5282649c12ad686428266013845
Enrique Horta <109767354+eh-globant@users.noreply.github.com>
2025-05-07 22:58:00 +0000

    Add grid wrapper and move logic

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,68 +1,17 @@
-import { useState, useEffect, useCallback } from "react";
-import Card from "../Card/Card";
-
 import { Product } from "../../types/product";
-import useInfiniteScroll from "../../hooks/useInfiniteScroll";
-import allProducts from "../../data/products";
-
+import Card from "../Card/Card";
 import styles from "./Grid.module.scss";
 
-export default function Grid() {
-  const [visibleProducts, setVisibleProducts] = useState<Product[]>([]);
-  const [page, setPage] = useState(0);
-  const [isLoading, setIsLoading] = useState(false);
-  const productsPerPage = 8;
+interface GridProps {
+  products: Product[];
+}
 
-  const loadMoreProducts = useCallback(() => {
-    if (isLoading) return;
-
-    setIsLoading(true);
-
-    // Simulates loading time
-    setTimeout(() => {
-      const nextPage = page + 1;
-      const startIndex = 0;
-      const endIndex = nextPage * productsPerPage;
-      const nextProducts = allProducts.slice(startIndex, endIndex);
-
-      setVisibleProducts(nextProducts);
-      setPage(nextPage);
-      setIsLoading(false);
-    }, 500);
-  }, [page, isLoading]);
-
-  // Initial loading
-  useEffect(() => {
-    loadMoreProducts();
-  }, []);
-
-  const targetRef = useInfiniteScroll(
-    loadMoreProducts,
-    visibleProducts.length < allProducts.length,
-    isLoading
-  );
-
+export default function Grid({ products }: GridProps) {
   return (
-    <div className={styles["grid-container"]}>
-      <div className={styles["grid"]}>
-        {visibleProducts.map((product) => (
-          <Card key={product.id} product={product} />
-        ))}
-      </div>
-
-      {/* Loading time and scroll triggering */}
-      <div ref={targetRef} className={styles["scroll-trigger"]}>
-        {isLoading && (
-          <div className={styles["loading-spinner"]}>Loading...</div>
-        )}
-      </div>
-
-      {/* If there are no more products */}
-      {!isLoading && visibleProducts.length >= allProducts.length && (
-        <div className={styles["no-more-products"]}>
-          There are no more products to show
-        </div>
-      )}
+    <div className={styles.grid}>
+      {products.map((product) => (
+        <Card key={product.id} product={product} />
+      ))}
     </div>
   );
 }
---
+++ src/components/Grid/GridWrapper.tsx
@@ -0,0 +1,63 @@
+import { useState, useEffect, useCallback } from "react";
+import Grid from "./Grid";
+import { Product } from "../../types/product";
+import useInfiniteScroll from "../../hooks/useInfiniteScroll";
+import allProducts from "../../data/products";
+import styles from "./Grid.module.scss";
+
+interface GridProps {
+  productsPerPage?: number;
+}
+
+export default function GridWrapper({ productsPerPage = 8 }: GridProps) {
+  const [visibleProducts, setVisibleProducts] = useState<Product[]>([]);
+  const [page, setPage] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+
+  // Load more products
+  const loadMoreProducts = useCallback(async () => {
+    if (isLoading) return;
+
+    setIsLoading(true);
+
+    // Simulating network delay
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    // Calculate indexes
+    const startIndex = page * productsPerPage;
+    const endIndex = startIndex + productsPerPage;
+    const nextProducts = allProducts.slice(startIndex, endIndex);
+
+    // Add new products
+    setVisibleProducts((prev) => [...prev, ...nextProducts]);
+    setPage((prevPage) => prevPage + 1);
+    setIsLoading(false);
+  }, [page, isLoading, productsPerPage]);
+
+  // Initial load
+  useEffect(() => {
+    // Load the first products at start (8 by default)
+    const initialProducts = allProducts.slice(0, productsPerPage);
+    setVisibleProducts(initialProducts);
+    setPage(1);
+  }, [productsPerPage]);
+
+  const targetRef = useInfiniteScroll(
+    loadMoreProducts,
+    visibleProducts.length < allProducts.length,
+    isLoading
+  );
+
+  return (
+    <>
+      <div className={styles["grid__wrapper"]}>
+        <Grid products={visibleProducts} />
+      </div>
+      <div ref={targetRef} className={styles["grid__scroll-trigger"]}>
+        {isLoading && (
+          <div className={styles["grid__loading-spinner"]}>Loading...</div>
+        )}
+      </div>
+    </>
+  );
+}
